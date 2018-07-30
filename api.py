66e0dab2aee188f68579aeff90dbca9f5fea500f
Dave Chen <totorochina@hotmail.com>
2018-07-30 13:00:09 +0800

    to v0.4

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         response = {
             "type": "app",
-            "version": "0.3"
+            "version": "0.4"
         }
         self.write(response)
 
